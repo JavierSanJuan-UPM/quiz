b9888b877dddaafc489eb840d34e935b04c33b6f
Javier San Juan Cervera <jedive@gmail.com>
2015-06-22 18:50:25 +0200

    Lista de preguntas. Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,21 +1,38 @@
 var models = require('../models/models.js');
 
-// GET /quizes/question
-exports.question = function(req, res) {
-  models.Quiz.findAll().success(function(quiz) {
-    res.render('quizes/question', {pregunta: quiz[0].pregunta});
-  });
+// Autoload - factoriza el codigo si la ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.find(quizId).then(
+    function(quiz) {
+      if ( quiz ) {
+        req.quiz = quiz;
+        next();
+      } else {
+        next(new Error('No existe quizId=' + quizId));
+      }
+    }
+  ).catch(function(error) { next(error); });
 };
 
-// GET /quizes/answer
+// GET /quizes/index
+exports.index = function(req, res, next) {
+  models.Quiz.findAll().then(function(quizes) {
+    res.render('quizes/index', { quizes: quizes });
+  }).catch(function(error) { next(error); });
+};
+
+// GET /quizes/:quizId
+exports.show = function(req, res) {
+  res.render('quizes/show', {quiz: req.quiz});
+};
+
+// GET /quizes/:quizId/answer
 exports.answer = function(req, res) {
-  models.Quiz.findAll().success(function(quiz) {
-    if ( req.query.respuesta === quiz[0].respuesta ) {
-      res.render('quizes/answer', {respuesta: 'Correcto'});
-    } else {
-      res.render('quizes/answer', {respuesta: 'Incorrecto'});
-    }
-  });
+  var resultado = 'Incorrecto';
+  if ( req.query.respuesta === req.quiz.respuesta ) {
+    resultado = 'Correcto';
+  }
+  res.render('quizes/answer', {quiz:req.quiz, respuesta: resultado});
 };
 
 // GET /author
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,20 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller.js');
 
-/* GET home page. */
+// Pagina de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
+
+// Definicion de rutas de quizes
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+
+// Definicion de ruta de creditos
 router.get('/author', quizController.author);
 
 module.exports = router;
