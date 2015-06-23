ae57ee926de354aa8d028e8e7fce271021f83d15
Javier San Juan Cervera <jedive@gmail.com>
2015-06-23 14:06:09 +0200

    Borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -108,6 +108,13 @@ exports.update = function(req, res) {
   }
 };
 
+// DELETE /quizes/:quizId
+exports.destroy = function(req, res) {
+  req.quiz.destroy().then(function(){
+    res.redirect('/quizes');
+  }).catch(function(error){ next(error); });
+};
+
 // GET /author
 exports.author = function(req, res) {
   res.render('author', { errors: [] });
